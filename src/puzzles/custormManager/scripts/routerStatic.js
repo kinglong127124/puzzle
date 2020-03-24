e9c3095649172a2578c47fb3dc2bda28e1ed3c42
zhengjl <12345678>
2020-03-24 16:31:12 +0800

    路由页面修改

--- src/puzzles/custormManager/scripts/routerStatic.js
+++ src/puzzles/custormManager/scripts/routerStatic.js
@@ -1,35 +1,35 @@
 export default [
   {
     name: "custormManagerIndex",
-    path: "/custormManager/index",
+    path: "custormManager/index",
     meta: { title: '客户管理-首页', icon: 'dashboard', isCache: true, affix: false },
     component: () =>
       import(/* webpackChunkName: "views/[request]" */ `../views/index`)
   },
   {
     name: "unitInfoDetail",
-    path: "/custormManager/id/:id*",
+    path: "custormManager/sysInfo/unitInfo/id/:id*",
     meta: { title: '客户注册-详情', icon: 'dashboard', isCache: true, affix: false },
     component: () =>
       import(/* webpackChunkName: "views/[request]" */ `../views/sysInfo/unitInfo/detail`)
   },
   {
-    path: 'businessContract/id/:id*',
+    path: 'custormManager/sysInfo/businessManager/businessContract/id/:id*',
     component: () => import(/* webpackChunkName: "views/[request]" */ '../views/sysInfo/businessManager/businessContract/detail'),
     name: 'businessContractDetail',
     meta: { title: '商务合同-详情', icon: 'dashboard', isCache: true, affix: false }
   }, {
-    path: 'resultsFile/id/:id*',
+    path: 'custormManager/sysInfo/businessManager/resultsFile/id/:id*',
     component: () => import(/* webpackChunkName: "views/[request]" */ '../views/sysInfo/businessManager/resultsFile/detail'),
     name: 'resultsFileDetail',
     meta: { title: '成果文件-详情', icon: 'dashboard', isCache: true, affix: false }
   }, {
-    path: 'serviceLog/id/:id*',
+    path: 'custormManager/sysInfo/businessManager/serviceLog/id/:id*',
     component: () => import(/* webpackChunkName: "views/[request]" */ '../views/sysInfo/businessManager/serviceLog/detail'),
     name: 'serviceLogDetail',
     meta: { title: '服务记录-详情', icon: 'dashboard', isCache: true, affix: false }
   }, {
-    path: 'expansion/id/:id*',
+    path: 'custormManager/sysInfo/businessManager/expansion/id/:id*',
     component: () => import(/* webpackChunkName: "views/[request]" */ '../views/sysInfo/businessManager/expansion/detail'),
     name: 'expansionDetail',
     meta: { title: '扩容处理-详情', icon: 'dashboard', isCache: true, affix: false }
--- src/puzzles/sysManager/scripts/routerStatic.js
+++ src/puzzles/sysManager/scripts/routerStatic.js
@@ -1,7 +1,7 @@
 export default [
   {
     name: "sysManagerIndex",
-    path: "/sysManager/index",
+    path: "sysManager/index",
     meta: { title: '系统管理-首页', icon: 'dashboard', isCache: true, affix: false },
     component: () =>
       import(/* webpackChunkName: "views/[request]" */ `../views/index`)
