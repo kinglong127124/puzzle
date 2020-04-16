e8f198453070c9a9f841ac1c17d8260b5276f25a
zhengjl <12345678>
2020-04-16 14:13:08 +0800

    增加操作人员详情

--- src/puzzles/sysManager/scripts/routerStatic.js
+++ src/puzzles/sysManager/scripts/routerStatic.js
@@ -5,5 +5,11 @@ export default [
     meta: { title: '系统管理-首页', icon: 'dashboard', isCache: true, affix: false },
     component: () =>
       import(/* webpackChunkName: "views/[request]" */ `../views/index`)
+  },
+  {
+    path: 'sysManager/groupAuth/userManager/id/:id*',
+    component: () => import(/* webpackChunkName: "views/[request]" */ '../views/groupAuth/userManager/detail'),
+    name: 'userManagerDetail',
+    meta: { title: '操作人员-详情', icon: 'dashboard', isCache: true, affix: false }
   }
 ];
