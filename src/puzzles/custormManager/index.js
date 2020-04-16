a75b6506863e8aaccf73315222b7de1191b74c1b
zhengjl <12345678>
2020-04-16 14:23:03 +0800

    删除冗余数据

--- src/puzzles/custormManager/index.js
+++ src/puzzles/custormManager/index.js
@@ -1,8 +1,7 @@
 import Router from "./scripts/router";
 import RouterStatic from "./scripts/routerStatic";
 import Store from "./store";
-// 模块样式
-import "./style/style.less";
+
 export default {
   router: Router,
   routerStatic: RouterStatic,
