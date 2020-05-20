a46ae3335de9e16bd2ce46941935471b32b4ac20
zhengjl <12345678>
2020-05-20 17:32:19 +0800

    dist修改

--- dist/map.js
+++ dist/map.js
@@ -1,5 +1,5 @@
 
     var modulesMap={
-      dashboard:"7e4d7b78474d2b2f188e",custormManager:"a0ac1645cf7d8fee6f12",sysManager:"7d6fde30d9b488f037b9",
+      dashboard:"7e4d7b78474d2b2f188e",custormManager:"41b1b36cee29216ec01a",sysManager:"071c37156491bd8256ce",
     }
   
