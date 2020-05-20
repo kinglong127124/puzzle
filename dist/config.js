ad1183853502b94834498654de4a078d792478a5
zhengjl <12345678>
2020-05-20 15:28:24 +0800

    增加dist

---
+++ dist/config.js
@@ -0,0 +1,13 @@
+// 系统配置
+window.PRODUCT_CONFIG = {
+  // 系统名称
+  appName: "超级管理",
+  // 接口地址
+  api: {
+    frame: "/api"
+  },
+  // 使用基座名称
+  frame: "dashboard",
+  // 指定一个首页，值为路由 name
+  pageHome: ""
+};
---
+++ dist/map.js
@@ -0,0 +1,5 @@
+
+    var modulesMap={
+      dashboard:"7e4d7b78474d2b2f188e",custormManager:"a0ac1645cf7d8fee6f12",sysManager:"7d6fde30d9b488f037b9",
+    }
+  
